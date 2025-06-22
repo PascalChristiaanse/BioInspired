39b1944f2db132db51c7466c5951b560fe968d43
PascalChristiaanse <pascal.christiaanse@gmail.com>
2025-06-22 19:43:19 +0200

    update module structure

--- src/bioinspired/__init__.py
+++ src/bioinspired/__init__.py
@@ -1,17 +1,11 @@
 """
 BioInspired: Evolutionary algorithms for automated docking procedures.
 """
-
-from .data import DatabaseManager, TrajectoryManager
-from .data.models import Environment, Algorithm, Individual, Trajectory, Annotation
-
 __version__ = "0.1.0"
 __all__ = [
-    "DatabaseManager", 
-    "TrajectoryManager",
-    "Environment", 
-    "Algorithm", 
-    "Individual", 
-    "Trajectory", 
-    "Annotation"
+    "algorithm",
+    "data",
+    "simulation",
+    "spacecraft",
+    "visualization",
 ]
