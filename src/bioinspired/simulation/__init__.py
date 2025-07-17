505e5a38df107c95572268399d93065348ce0bcf
PascalChristiaanse <pascal.christiaanse@gmail.com>
2025-07-17 23:58:03 +0200

    add earth universe simulator

--- src/bioinspired/simulation/__init__.py
+++ src/bioinspired/simulation/__init__.py
@@ -3,8 +3,10 @@ simulation environment for the bioinspired package.
 """
 
 from .empty_universe import EmptyUniverseSimulator
+from .earth import EarthSimulator
 
 # Ensure imports are available in the package namespace
 __all__ = [
     "EmptyUniverseSimulator",
+    "EarthSimulator",
 ]
