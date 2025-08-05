4a2dafb74a34c49b8d6b2c093c4d3cbe50e6c7de
PascalChristiaanse <pascal.christiaanse@gmail.com>
2025-08-06 00:02:45 +0200

    add CostFunctionBase to module exports

--- src/bioinspired/problem/__init__.py
+++ src/bioinspired/problem/__init__.py
@@ -3,6 +3,11 @@ This module provides a base class for PyGMO problems and specific implementation
 """
 
 from .problem_base import ProblemBase
-from .cost_functions import JLeitner2010NoStopNeuron, JLeitner2010
+from .cost_functions import JLeitner2010NoStopNeuron, JLeitner2010, CostFunctionBase
 
-__all__ = ["ProblemBase", "JLeitner2010NoStopNeuron", "JLeitner2010"]
+__all__ = [
+    "ProblemBase",
+    "JLeitner2010NoStopNeuron",
+    "JLeitner2010",
+    "CostFunctionBase",
+]
