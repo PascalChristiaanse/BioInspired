55343b2016f8097aca0f907f44e28231e74d4c51
PascalChristiaanse <53832346+PascalChristiaanse@users.noreply.github.com>
2025-08-09 15:46:58 +0200

    add SliceablePopulation to module exports

--- src/bioinspired/algorithm/__init__.py
+++ src/bioinspired/algorithm/__init__.py
@@ -3,7 +3,9 @@ Algorithm modules for evolutionary computation.
 """
 
 from .initializer_BFE import InitializerBFE
+from .sliceable_population import SliceablePopulation
 
 __all__ = [
     "InitializerBFE",
+    "SliceablePopulation", 
 ]
